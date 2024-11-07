8aceca87369fb2891ffcb66610344632c50f2479
unknown <Ahliddin dev>
2024-11-07 11:51:47 +0500

    project 1

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("write number :");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var  ones = number % 10;
+            var  tens = number / 10 % 10;
+            var  hunderds = number / 100 % 10;
+            var  thousands = number / 1000 % 10;
+            var result = ones + tens + hunderds + thousands;
+            Console.WriteLine(result);
+
         }
     }
 }
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+508a30e62c402dbd1c2ae9a7cfd81ccaf170c073")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
